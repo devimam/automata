1ce3ee382caf383ac58be4b2dc4209af50c16459
Mohammad Imam Hossain <37970985+devimam@users.noreply.github.com>
2020-12-14 02:39:10 +0600

    nfa to dfa conversion js

--- js/myscript.js
+++ js/myscript.js
@@ -66,6 +66,24 @@ All rights reserved.
         }
     });
 
+    ///---------------------------------dragging nodes--------------------------------
+
+    // Initialize the dragNodes plugin:
+    var dragListener = sigma.plugins.dragNodes(s, s.renderers[0]);
+
+    dragListener.bind('startdrag', function(event) {
+      console.log(event);
+    });
+    dragListener.bind('drag', function(event) {
+      console.log(event);
+    });
+    dragListener.bind('drop', function(event) {
+      console.log(event);
+    });
+    dragListener.bind('dragend', function(event) {
+      console.log(event);
+    });
+
     ///-------------------------------state creation-----------------------------------
     /// creating new node on a single click in the stage area
     s.bind('clickStage', createnewstate);
@@ -458,24 +476,6 @@ All rights reserved.
         resetedgemodal();
     }
 
-    ///---------------------------------dragging nodes--------------------------------
-
-    // Initialize the dragNodes plugin:
-    var dragListener = sigma.plugins.dragNodes(s, s.renderers[0]);
-
-    dragListener.bind('startdrag', function(event) {
-      console.log(event);
-    });
-    dragListener.bind('drag', function(event) {
-      console.log(event);
-    });
-    dragListener.bind('drop', function(event) {
-      console.log(event);
-    });
-    dragListener.bind('dragend', function(event) {
-      console.log(event);
-    });
-
     ///------------------------------js object builder----------------------------------
     function buildobj(){
         var obj=new Object();
@@ -514,6 +514,8 @@ All rights reserved.
                     comp5arr.push(tmpstateid);
                 }
             }
+            comp1arr.sort();
+            comp5arr.sort();
 
             if(startstatecount==1){
                 ///only 1 start state found
@@ -597,7 +599,11 @@ All rights reserved.
     }
 
     ///------------------------------showing transition table on request--------------------
-    document.getElementById('showtrtable').addEventListener('click',showtransitiontable);
+    ///document.getElementById('showtrtable').addEventListener('click',showtransitiontable);
+    $('#collapsesection').on('hide.bs.collapse', function(){
+        document.getElementById('tablecontainer').innerHTML="";
+    });
+    $('#collapsesection').on('show.bs.collapse', showtransitiontable);
     function showtransitiontable(e){
         console.log('showing transtion table');
         console.log(e);
@@ -611,7 +617,7 @@ All rights reserved.
                     tablestring+="<th></th>"; ///adding first empty column
                     for(var ind in stateobj['comp2']){
                         var cursymbol=stateobj['comp2'][ind];
-                        tablestring+="<th>"+cursymbol+"</th>"; ///adding 1 column per alhapbet symbol
+                        tablestring+="<th><b><code>"+cursymbol+"</b></code></th>"; ///adding 1 column per alhapbet symbol
                     }
                 tablestring+="</tr></thead>";
 
@@ -663,21 +669,26 @@ All rights reserved.
             document.getElementById('tablecontainer').innerHTML=tablestring;
 
             ///hiding the collapsible pane section
-            $('.collapse').collapse('toggle');
+            ///$('.collapse').collapse('toggle');
         }
         else{
             console.log(stateobj['errors']);
+            document.getElementById('tablecontainer').innerHTML="Invalid state diagram.";
             ///hiding the collapsible pane section
-            $('.collapse').collapse('hide');
-            window.alert("Invalid State Diagram.");
+            ///$('.collapse').collapse('hide');
+            ///window.alert("Invalid State Diagram.");
         }
     }
 
     ///--------------------formal definition generation----------------------------
-    document.getElementById('generatebtn').addEventListener('click',generateformaldef);
+    $('#formaldefcollapse').on('show.bs.collapse',generateformaldef);
     function generateformaldef(e){
+        document.getElementById('showbtnlabel').innerText='Hide';
         var stateobj=buildobj();
         if(stateobj.errors.length==0){
+            document.getElementById('formaldeferror').innerHTML="";
+            document.getElementById('tableportion').style.display="block";
+
             document.getElementById('comp1').innerText=stateobj['comp1'].map(s.graph.getNodeLabel).join(',');
             document.getElementById('comp2').innerText=stateobj['comp2'].filter((item, i, ar) => (item==String.fromCharCode(949)) ? false : true).join(',');
             document.getElementById('comp4').innerText=s.graph.getNodeLabel(stateobj['comp4']);
@@ -685,9 +696,18 @@ All rights reserved.
         }
         else{
             console.log(stateobj['errors']);
-            window.alert("Invalid State Diagram.");
+            document.getElementById('formaldeferror').innerHTML="Invalid state diagram";
+            document.getElementById('tableportion').style.display="none";
         }
     }
+
+    $('#formaldefcollapse').on('hide.bs.collapse',hideformaldef);
+    function hideformaldef(e){
+        document.getElementById('showbtnlabel').innerText='Show';
+        ///also hiding the transitiont table collapsible pane if opened
+        $('#collapsesection').collapse('hide');
+    }
+
 
     ///-------------------input simulation string-----------------------------------------------------------
     document.getElementById('simulatebtn').addEventListener('click', simulatemodal);
@@ -698,12 +718,357 @@ All rights reserved.
         var stateobj=buildobj();
         if(stateobj['errors'].length==0){
             ///valid graph
-
-
+            window.alert('Still in development phase');
         }
         else{
             console.log(stateobj['errors']);
             window.alert("Invalid State Diagram.");
         }
     }
+
+    ///-------------------NFA to DFA covnersion-------------------------------------------------------------
+    document.getElementById('nfatodfabtn').addEventListener('click', nfatodfaconvert);
+    function nfatodfaconvert(e){
+        console.log('starting nfa to dfa conversion');
+        console.log(e);
+
+        var stateobj=buildobj();
+        if(stateobj['errors'].length==0){ ///valid graph
+            var dfaobj=new Object(); ///this object will hold the main dfa object
+
+            ///dfaobj start state = epsilon closure of nfa start state
+            dfaobj['comp4']=epsilonClosure(stateobj['comp4'], stateobj['comp3']);
+            dfaobj['comp4'].sort();
+            console.log(dfaobj['comp4']);
+            console.log('Dfa start state: {'+dfaobj['comp4'].join(',')+" }");
+
+            ///dfaobj alphabets are the same as nfa alphabets except epsilon
+            dfaobj['comp2']=stateobj['comp2'].filter((item,i,ar) => (item==String.fromCharCode(949) ? false : true));
+            dfaobj['comp2'].sort();
+            console.log('Dfa alphabet: '+dfaobj['comp2'].join(','));
+
+            var transitiontable=new Object(); ///to save the JS object
+
+            dfaobj['comp1']=[]; ///array of states, remember each state may contain more than one nfa state i.e. each state is also an array
+            dfaobj['comp1'].push(dfaobj['comp4']); ///first starting from the dfa start state
+
+            var newstatequeue=[];
+            newstatequeue.push(dfaobj['comp4']);
+            while(newstatequeue.length>0){
+                var curnewstate=newstatequeue.shift(); ///extracting unvisited dfa states
+                transitiontable[curnewstate]=new Object();
+
+                ///for each symbol calculating the destination state
+                for(var ind in dfaobj['comp2']){
+                    var cursymbol=dfaobj['comp2'][ind];
+                    console.log("checking for: "+curnewstate.join(',')+" , "+cursymbol);
+
+                    var destination=destinationstate(curnewstate, cursymbol, stateobj['comp3']); ///1 single state containing multiple nfa states
+                    if(destination.length==0){
+                        destination=['<emptyset>'];
+                    }
+
+                    console.log('For '+curnewstate.join(',')+' , '+cursymbol+' destination is: '+destination.join(','));
+                    transitiontable[curnewstate][cursymbol]=destination;
+
+                    ///if the state doesn't already exists then add the state to the newstatequeue for further processing
+                    var isfound=false;
+                    for(var eachstateind in dfaobj['comp1']){
+                        var tmpstate=dfaobj['comp1'][eachstateind];
+                        if(arraysEqual(tmpstate,destination)==true){
+                            isfound=true;
+                            break;
+                        }
+                    }
+                    if(isfound==false){
+                        ///new state found
+                        console.log('new state found '+destination);
+                        newstatequeue.push(destination);
+                        dfaobj['comp1'].push(destination);
+                    }
+
+                }
+            }
+            console.log("Transition Table:");
+            console.log(transitiontable);
+            dfaobj['comp3']=transitiontable; ///component 3 calculated
+
+            var nfafinal=stateobj['comp5'];
+            dfaobj['comp5']=[];
+            for(var ind in dfaobj['comp1']){
+                var curstate=dfaobj['comp1'][ind];
+                console.log(nfafinal);
+                console.log(curstate);
+                var commonstates = nfafinal.filter(item => curstate.includes(item));
+                if(commonstates.length>0){
+                    ///at least 1 states of nfa final state exists here
+                    dfaobj['comp5'].push(curstate);
+                }
+            }
+
+
+            ////showing the full dfa converted object
+            console.log('full dfa converted object');
+            console.log(dfaobj);
+
+            ///drawing the dfa
+            drawdfacontainer(dfaobj);
+        }
+        else{
+            console.log(stateobj['errors']);
+            document.getElementById('nfatodfapanel').innerHTML="<span style='color:red;'>Invalid State Diagram</span>";
+        }
+    }
+
+    ///drawnfacontainer
+    function drawdfacontainer(dfaobj){
+        ///drawing the dfa within the nfatodfapanel
+        console.log('drawing corresponding dfa diagram');
+
+        document.getElementById('nfatodfapanel').innerHTML="";
+
+        ///initializing new sigma instance
+        var s1=new sigma({
+            id: 'sigmaappdfa'+Date.now(),
+            renderers: [
+                {
+                    container: document.getElementById('nfatodfapanel'),
+                    type: 'canvas',
+                }
+            ],
+            settings:{
+                autoRescale: true,
+                doubleClickEnabled: false,
+                enableCamera: true,
+                enableEdgeHovering: true, ///the edges can be hovered to display special rendering adn detial text
+                edgeHoverExtremities: false,
+
+                defaultLabelColor: borderandtextColor, // to change the label color
+                defaultLabelSize: 16, // to change the label size
+                defaultLabelHoverColor: borderandtextColor, // the default text color of hovered labels
+                defaultHoverLabelBGColor: labelbgColor, // default background color of hovered nodes labels
+
+                defaultNodeColor: stateColor, // to change the node color
+                borderSize: 2, // the size of the border of hovered nodes
+                defaultNodeBorderColor: 'transparent',
+
+                defaultEdgeColor: borderandtextColor,
+                edgeColor: 'default',
+                maxEdgeSize: 4,
+                defaultEdgeHoverColor: '#803300',
+                edgeHoverColor: 'default',
+
+                defaultEdgeLabelColor: borderandtextColor,
+                defaultEdgeLabelSize: 20,
+            }
+        });
+
+        // Initializing the dragNodes plugin:
+        var dragListener = sigma.plugins.dragNodes(s1, s1.renderers[0]);
+
+        dragListener.bind('startdrag', function(event) {
+          console.log(event);
+        });
+        dragListener.bind('drag', function(event) {
+          console.log(event);
+        });
+        dragListener.bind('drop', function(event) {
+          console.log(event);
+        });
+        dragListener.bind('dragend', function(event) {
+          console.log(event);
+        });
+
+        ///adding nodes
+        var dfastates=dfaobj['comp1'];
+        for(var ind in dfastates){
+            var statearr=dfastates[ind];
+            var statelabel;
+            if(statearr[0]=="<emptyset>") statelabel="{ }";
+            else statelabel="{"+statearr.map(s.graph.getNodeLabel).join(',')+'}';
+
+            var statetype='def';
+            ///checking for state types
+            var isfinal=false;
+            for(var ind1 in dfaobj['comp5']){
+                var curstate=dfaobj['comp5'][ind1];
+                if(arraysEqual(curstate, statearr)==true){
+                    isfinal=true;
+                    break;
+                }
+            }
+            console.log(isfinal);
+
+            if(arraysEqual(statearr, dfaobj['comp4'])==true && isfinal==true){
+                statetype='bothstate';
+            }
+            else if(arraysEqual(statearr, dfaobj['comp4'])==true){
+                statetype='startstate';
+            }
+            else if(isfinal==true){
+                statetype='finalstate';
+            }
+
+            s1.graph.addNode({
+                id: statearr.join(','), ///using the main ids for id
+                label: statelabel, ///using the labels for label
+                x: Math.random(),
+                y: Math.random(),
+                size: stateSize,
+                type: statetype
+            });
+        }
+        console.log(s1.graph.nodes());
+
+        ///adding edges
+        var transitiontable=dfaobj['comp3'];
+        for(var ind in dfastates){
+            var srcstate=dfastates[ind];
+            var srcstateid=srcstate.join(',');
+            if(srcstate.length==0) srcstateid="<emptyset>";
+
+            for(var ind1 in dfaobj['comp2']){
+                var symbol=dfaobj['comp2'][ind1];
+
+                var deststate=transitiontable[srcstate][symbol];
+                var deststateid=deststate.join(',');
+                if(deststate.length==0) deststateid="<emptyset>";
+
+                console.log(srcstateid+" --- "+ symbol+" --- "+deststateid);
+
+                var edgeid=srcstateid+"-"+deststateid;
+                ///already this edge exists checking
+                var alreadyexists=false;
+                var reverseexists=false;
+                for(var ind2 in s1.graph.edges()){
+                    var curedge=s1.graph.edges()[ind2];
+                    if(curedge.id==edgeid){
+                        curedge.label=curedge.label+", "+symbol;
+                        alreadyexists=true;
+                    }
+                    if(curedge.source==deststateid && curedge.target==srcstateid){
+                        reverseexists=true;
+                        curedge.type='curvedArrow';
+                    }
+                }
+
+                var edgetype='arrow';
+                if(srcstateid==deststateid || reverseexists==true){
+                    edgetype='curvedArrow';
+                }
+
+                if(alreadyexists==false){
+                    s1.graph.addEdge({
+                        id: edgeid,
+                        label: symbol,
+                        source: srcstateid,
+                        target: deststateid,
+                        size: 3,
+                        type: edgetype
+                    });
+                }
+            }
+        }
+
+        // Finally, let's ask our sigma instance to refresh:
+        s1.refresh();
+    }
+
+    ///calculate destination state
+    function destinationstate(statearr, symbol, transitiontable){ //here statearr contains 1 dfa states that is a combination of multiple nfa states
+        var destination=[];
+        var epsilondestination=[]; ///epsilon destination is the final main destination state
+
+        for(var ind in statearr){
+            var stateportion=statearr[ind];
+
+            ///checking whether entry exists for this stateportion and this symbol
+            var statekeys=Object.keys(transitiontable);
+            if(statekeys.indexOf(stateportion)!=-1){
+                ///checking whether transition exists for the symbol
+                var symbolkeys=Object.keys(transitiontable[stateportion]);
+                if(symbolkeys.indexOf(symbol)!=-1){
+                    destination=destination.concat(transitiontable[stateportion][symbol]);
+                }
+                else{
+                    ///no transition exists
+                }
+            }
+            else{
+                ///no transition exists
+            }
+        }
+
+        ///making destination elements unique
+        destination=destination.filter((item,i,ar) => (ar.indexOf(item)==i ? true : false));
+        ///console.log('destination: '+destination.join(','));
+
+        ///calculating epsilon closure for each state from destination
+        var finaldestination=destination;
+        for(var ind in destination){
+            var tmpstate=destination[ind];
+            finaldestination=finaldestination.concat(epsilonClosure(tmpstate, transitiontable));
+        }
+        ///making final destination elements unique
+        finaldestination=finaldestination.filter((item,i,ar) => (ar.indexOf(item)==i ? true : false));
+        ///console.log('epsilon closure of destination: '+finaldestination.join(','));
+
+        return finaldestination.sort();
+    }
+
+    ///two array equality checking
+    function arraysEqual(a, b) {
+      if (a === b) return true;
+      if (a == null || b == null) return false;
+      if (a.length !== b.length) return false;
+
+      for (var i = 0; i < a.length; ++i) {
+        if (a[i] !== b[i]) return false;
+      }
+      return true;
+    }
+
+    ///calculation epsilon closure of state named 'st'
+    function epsilonClosure(st, transitiontable){
+        ///st = the state to calculate the epsilon closure
+        ///stateobj = full statediagram JS object
+
+        ///empty set epsilon closure is also emply
+        if(st=="") return [];
+
+        var result=[];
+        result.push(st); /// first epsilon closure of st is st itself
+
+        var queue=[];
+        queue.push(st);
+        while(queue.length!=0){
+            var poppedstate=queue.shift();
+            ///checking whether keys exists within transitiontable
+            var statekeys=Object.keys(transitiontable);
+            if(statekeys.indexOf(poppedstate)!=-1){
+                ///now checking whether entry exists for epsilon symbol
+                var symbolkeys=Object.keys(transitiontable[poppedstate]);
+                if(symbolkeys.indexOf(String.fromCharCode(949))!=-1){
+                    ///entry exists. so adding the element within the result and queue object
+                    var newstate=transitiontable[poppedstate][String.fromCharCode(949)]; ///it's an array
+
+                    for(var eachstind in newstate){
+                        result.push(newstate[eachstind]);
+                        queue.push(newstate[eachstind]);
+                    }
+                }
+                else{
+                    ///nothing to do
+                }
+            }
+            else{
+                ///nothing to do
+            }
+        }
+
+        ///console.log('epsilon closure of '+st+" is: ");
+        ///console.log(result);
+        return result.sort();
+    }
+
 })();
